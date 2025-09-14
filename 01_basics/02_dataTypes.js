2515b50fbf66b5fabef33447c2b20d75bdb5c3dd
BoragePratiksha <139120885+BoragePratiksha@users.noreply.github.com>
2025-09-14 11:39:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+let name = "Pratiksha" //String
+let age = 18
+let isLoggedIn = false
+
+//number 
+//bigint
+//String
+// boolean
+
+console.log(typeof(null));
+
