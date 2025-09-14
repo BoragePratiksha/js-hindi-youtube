6926f69cffcd4e4172b771039839acc50ba982de
BoragePratiksha <139120885+BoragePratiksha@users.noreply.github.com>
2025-09-14 10:47:57 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail ="pratiksha@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+accountEmail = "hp@hp@"
+accountPassword = "1908"
+accountCity = "bengluru"
+console.log(accountEmail);
+console.table([accountId , accountEmail , accountPassword,accountCity])
